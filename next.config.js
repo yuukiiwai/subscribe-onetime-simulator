15b0fe1f63c0fc4d615eddcee98c1db2780e494a
yuukiiwai <yuuki.20020217@gmail.com>
2023-09-25 12:49:42 +0900

    add not github page

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const branchName = process.env.BRANCH_NAME ? "/" + process.env.BRANCH_NAME : "";
-
-const nextConfig = {
-    reactStrictMode: true,
-    assetPrefix: branchName,
-    basePath: branchName,
-};
+const nextConfig = {}
 
 module.exports = nextConfig
